fb97479f67dbfaf99e025128b8d6db3a2abe6f4c
supriyakumarikeshri <supriyakumari2393@gmail.com>
2020-05-25 14:24:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,5 +54,14 @@ class App extends React.Component {
 }
 
  //{data.map(buildRow)}
- 
+//  const a = [1, 10, 100, 1000, 10000];
+// const Repeater = () => {
+//   return (
+//     <ul>
+//       {a.map(i => {
+//         return <li>{i}</li>;
+//       })}
+//     </ul>
+//   );
+// };
 export default App;
